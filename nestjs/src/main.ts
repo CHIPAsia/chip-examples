31c2198b1bfc34acc1766b4df66424509c2bfce8
Ahmad Azuddin <exectraquasar@gmail.com>
2022-12-28 12:55:43 +0800

    add session to main

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -2,12 +2,20 @@ import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { AppModule } from "./app.module";
 import { resolve } from "path";
+import * as session from 'express-session';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     rawBody: true,
   });
 
+  app.use(
+    session({
+      secret: 'my-secret',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
   app.useStaticAssets(resolve("./public"));
   app.setBaseViewsDir(resolve("./views"));
   app.setViewEngine("hbs");
